b996603abe8a88d6bc9772627c556bdc73b2fcf9
Harry Dodson <harry.lee.dodson@gmail.com>
2023-04-17 14:45:02 +0000

    updated register and sign in pages to retrieve from node server.

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Register = ({ onRouteChange }) => {
+const Register = ({ onRouteChange, loadUser }) => {
+    
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const registerUser = () => {
+        fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: name, email: email, password: password })
+        })
+            .then(res => res.json())
+            .then(user => {
+                if(user) {
+                    loadUser(user)
+                    onRouteChange('home')
+                }
+            })
+            .catch(error => console.log('oopsie', error))
+    }
+
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
@@ -9,19 +30,19 @@ const Register = ({ onRouteChange }) => {
                         <legend className="f1 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
+                            <input onChange={(e) => setName(e.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
                         </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="reg-email-address" />
+                            <input onChange={(e) => setEmail(e.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="reg-email-address" />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="reg-password" />
+                            <input onChange={(e) => setPassword(e.target.value)} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="reg-password" />
                         </div>
                     </fieldset>
                     <div className="">
-                        <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign up" />
+                        <input onClick={() => registerUser()} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign up" />
                     </div>
                 </div>
             </main>
--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SignIn = ({ onRouteChange }) => {
+const SignIn = ({ onRouteChange, loadUser }) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitSignIn = () => {
+        // let success = false
+        fetch('http://localhost:3000/signin', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password })
+        })
+            .then(res => res.json())
+            .then(user => {
+                if (user.id) {
+                    loadUser(user)
+                    onRouteChange('home')
+                }
+            })
+    }
+
     return (
         <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
                 <div className="measure">
                     <fieldset id="sign_in" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                        <div className="mt3">
+                        <div className="mt3" onChange={(e) => setEmail(e.target.value)}>
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                         </div>
-                        <div className="mv3">
+                        <div className="mv3" onChange={(e) => setPassword(e.target.value)}>
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                             <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
                         </div>
                     </fieldset>
                     <div className="">
-                        <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                        <input onClick={() => onSubmitSignIn()} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                     </div>
                     <div className="lh-copy mt3">
                         <p onClick={() => onRouteChange('register')} href="#0" className="f6 link dim black db pointer">Register</p>
