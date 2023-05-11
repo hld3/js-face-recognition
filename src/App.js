c30e88b48c6c4991365a9a16a4d033e6d205b492
Harry Dodson <harry.lee.dodson@gmail.com>
2023-05-11 16:46:59 +0000

    moved clarafai connection to the backend.

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ import Rank from './components/rank/Rank';
 import Register from './components/register/Register';
 import SignIn from './components/signIn/SignIn';
 
-const USER_ID = 'hld3';
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = '2e4a2196676d4e4f8e0dee0b12b14b62';
-const APP_ID = 'd2c92fdc722b4142aa5509a365a10a4c';
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection';
-const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-let IMAGE_URL = '';
-
-///////////////////////////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-///////////////////////////////////////////////////////////////////////////////////
-
 const initialState = {
   input: '',
   imageUrl: '',
@@ -35,35 +22,6 @@ const initialState = {
     entries: 0,
     joined: '',
   },
-};
-
-const constructRaw = () => {
-  return JSON.stringify({
-    user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-};
-
-const requestOptions = (raw) => {
-  return {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'Key ' + PAT,
-    },
-    body: raw,
-  };
 };
 
 class App extends Component {
@@ -118,17 +76,12 @@ class App extends Component {
 
   onDetectClick = () => {
     this.setState({ imageUrl: this.state.input });
-    IMAGE_URL = this.state.input;
-    const raw = constructRaw();
 
-    fetch(
-      'https://api.clarifai.com/v2/models/' +
-        MODEL_ID +
-        '/versions/' +
-        MODEL_VERSION_ID +
-        '/outputs',
-      requestOptions(raw)
-    )
+    fetch('http://localhost:3000/image_data', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ input: this.state.input }),
+    })
       .then((response) => response.json())
       .then((result) => {
         if (result) {
@@ -139,7 +92,6 @@ class App extends Component {
           })
             .then((res) => res.json())
             .then((count) => {
-              console.log(count);
               this.setState(Object.assign(this.state.user, { entries: count }));
             })
             .catch((error) => console.log(error));
