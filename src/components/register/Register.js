45bc5a3f652839dd8ca88447fb1411b7b80148d8
Harry Dodson <harry.lee.dodson@gmail.com>
2023-05-13 16:39:37 +0000

    fixed register to not log in without an actual user

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,53 +1,81 @@
 import React, { useState } from 'react';
 
 const Register = ({ onRouteChange, loadUser }) => {
-    
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const registerUser = () => {
-        fetch('http://localhost:3000/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ name: name, email: email, password: password })
-        })
-            .then(res => res.json())
-            .then(user => {
-                if(user) {
-                    loadUser(user)
-                    onRouteChange('home')
-                }
-            })
-            .catch(error => console.log('oopsie', error))
-    }
+  const registerUser = () => {
+    fetch('http://localhost:3000/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: name, email: email, password: password }),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        if (user.id) {
+          loadUser(user);
+          onRouteChange('home');
+        }
+      })
+      .catch((error) => console.log('oopsie', error));
+  };
 
-    return (
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Register</legend>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                            <input onChange={(e) => setName(e.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
-                        </div>
-                        <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input onChange={(e) => setEmail(e.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="reg-email-address" />
-                        </div>
-                        <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input onChange={(e) => setPassword(e.target.value)} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="reg-password" />
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input onClick={() => registerUser()} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign up" />
-                    </div>
-                </div>
-            </main>
-        </article>
-    )
-}
+  return (
+    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+              />
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="reg-email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="reg-password"
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={() => registerUser()}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Sign up"
+            />
+          </div>
+        </div>
+      </main>
+    </article>
+  );
+};
 
 export default Register;
